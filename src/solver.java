be1e94f170e990a10a35e8145854632dc94d0438
Pranav <saipranav1289@gmail.com>
2020-06-10 16:22:59 -0400

    Added the solver method, which solves the sudoku puzzle if it is valid

--- src/solver.java
+++ src/solver.java
@@ -1,21 +1,18 @@
 public class solver {
 
     public static boolean validEntry(int[][] sudoku, int row, int col) {
-        return checkRow(sudoku, col, row) && checkCol(sudoku, col, row) && checkBox(sudoku, col, row);
+        return (checkRow(sudoku, row, col) && checkCol(sudoku, row, col)) && checkBox(sudoku, row, col);
     }
 
     public static boolean checkRow(int[][] sudoku, int row, int col) {
 
         for (int i = 0; i < 9; i++) {
-
             if (i == col) {
                 continue;
             }
-
             if (sudoku[row][i] == sudoku[row][col]) {
                 return false;
             }
-
         }
 
         return true;
@@ -24,15 +21,12 @@ public class solver {
     public static boolean checkCol(int[][] sudoku, int row, int col) {
 
         for (int i = 0; i < 9; i++) {
-
             if (i == row) {
                 continue;
             }
-
             if (sudoku[i][col] == sudoku[row][col]) {
                 return false;
             }
-
         }
 
         return true;
@@ -44,19 +38,14 @@ public class solver {
         int colIndex = col / 3;
 
         for (int y = 0; y < 3; y++) {
-
             for (int x = 0; x < 3; x++) {
-
                 if (y + (3*rowIndex) == row && x + (3*colIndex) == col) {
                     continue;
                 }
-
                 if (sudoku[y + (3*rowIndex)][x + (3*colIndex)] == sudoku[row][col]) {
                     return false;
                 }
-
             }
-
         }
 
         return true;
@@ -75,6 +64,44 @@ public class solver {
         }
     }
 
+    public static boolean solve(int[][] sudoku) {
+
+        int x = -1;
+        int y = -1;
+        boolean stop = false;
+
+        for (int row = 0; row < 9 && !stop; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (sudoku[row][col] == 0) {
+                    x = row;
+                    y = col;
+                    stop = true;
+                    break;
+                }
+            }
+        }
+
+        if (x == -1) {
+            return true;
+        }
+
+        for (int i = 1; i < 10; i++) {
+
+            sudoku[x][y] = i;
+
+            if (validEntry(sudoku, x, y)) {
+                if (solve(sudoku)) {
+                    return true;
+                }
+            }
+
+            sudoku[x][y] = 0;
+
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
 
         int[][] sudoku = {
@@ -90,9 +117,14 @@ public class solver {
 
         printSudoku(sudoku);
 
-        //System.out.println(checkBox(sudoku, 4, 7));
+        System.out.println("");
 
-        System.out.println("\n\n");
+        if (solve(sudoku)) {
+            System.out.println("Solution found\n");
+            printSudoku(sudoku);
+        } else {
+            System.out.println("Invalid Sudoku");
+        }
         
     }
 
